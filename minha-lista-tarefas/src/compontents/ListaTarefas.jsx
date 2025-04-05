7a40be798e4afd360c9b75a5515c52fbcce45aa8
gwerta <glimacarniatto@gmail.com>
2025-04-05 18:42:41 -0300

    acaboooo

--- minha-lista-tarefas/src/compontents/ListaTarefas.jsx
+++ minha-lista-tarefas/src/compontents/ListaTarefas.jsx
@@ -17,25 +17,50 @@ function ListaTarefas() {//cria a função ListaTarefas
     const removerTarefa = (indice) => {//cria a arrow function de remover uma tarefa adicionando um índice para cada tarefa
         setTarefas(tarefas.filter((_, i) => i !== indice));//remove a tarefa filtrando-a pelo indice
     };
-    //muda a ordem de normal pra alfabetica
-    const mudaOrdem = () => {
+    
+    const mudaOrdem = () => {//muda a ordem de normal pra alfabetica
         setOrdemAlfabetica(!ordemAlfabetica);
     };
-    //cria a função de concluir a tarefa
-    const concluida = (indice) => {
+    
+    const concluida = (indice) => {//cria a função de concluir a tarefa
       const novasTarefas = [...tarefas];
       novasTarefas[indice].concluida = !novasTarefas[indice].concluida; // Alterna o estado de concluída
       setTarefas(novasTarefas);
     };
-  
+
+     
+     useEffect(() => {// converte as arrays em string e salva no localstorage
+        if (tarefas.length > 0) {
+        localStorage.setItem("tarefas", JSON.stringify(tarefas)); 
+        }
+    }, [tarefas]); 
+
+    
+  const buscarTarefas = () => {// Função para buscar tarefas do localStorage
+    const tarefasSalvas = localStorage.getItem("tarefas");  
+    if (tarefasSalvas) {
+      return JSON.parse(tarefasSalvas); //converte o json em string
+    }
+    return []; 
+  };
+
+//atualiza a pagina ja com as tarefas carregadas
+    useEffect(() => {
+        const tarefasCarregadas = buscarTarefas();
+        setTarefas(tarefasCarregadas);
+    },
+         []);
+
 
     return (//possibilita o retorno da interface na tela
 
         <div className="lateral">
-        <div className='esquerda'> 
-        <button class='add' onClick={mudaOrdem}><span className="text"> 
-        {ordemAlfabetica ?"Mostrar ordem normal" : "Mostrar ordem alfabética"}</span>
-      </button>
+             <div className='esquerda'> 
+        <button class='add' onClick={mudaOrdem}>
+            <span className="text"> 
+                 {ordemAlfabetica ?"Mostrar ordem normal" : "Mostrar ordem alfabética"}
+            </span>
+         </button>
             
           {/* cria o input com o valor do que você quer inserir na nova tarefa */}
             <input
@@ -48,9 +73,9 @@ function ListaTarefas() {//cria a função ListaTarefas
             />
             <button className='add' onClick={adicionarTarefa}><span className="text">Adicionar</span></button> {/*botão de adicionar com a funçao de adicionar um atarefa */}
            
-            </div>
+             </div>
             <div className='direita'>
-            <h2>Lista de Tarefas</h2> {/* adiciona o titulo para a página */}
+                 <h2>Lista de Tarefas</h2> {/* adiciona o titulo para a página */}
             <ul>
                {/*lista a tarefa com a possibilidade de mudar a ordem original ou alfabética mantendo as tarefas checkadas */} 
                {(ordemAlfabetica ? [...tarefas].sort((a, b) => a.nome.localeCompare(b.nome)) : tarefas).map((tarefa, indice) => (
@@ -61,13 +86,21 @@ function ListaTarefas() {//cria a função ListaTarefas
                             checked={tarefa.concluida}
                             onChange={() => concluida(indice)}
                         />
-                        {tarefa.nome}
+                            <span
+                                style={{
+                                    textDecoration: tarefa.concluida ? "line-through" : "none",
+                                    color: tarefa.concluida  ? "gray" : "#070064" // Muda a cor do texto e adiciona uma linha quando concluido
+                                }}
+                                >
+                                 {tarefa.nome}
+                             </span>
+                        
                         <button className='remo' onClick={() => removerTarefa(indice)}>x</button>
                     </li>
 ))}
             </ul>
             </div>
-        </div>
+     </div>
     );
 }
 
